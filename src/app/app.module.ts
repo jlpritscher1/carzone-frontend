4e4bc8c4da00823523448829c543c5497a390c98
Lucas Lang <lucaslang8@gmail.com>
2019-06-12 10:32:26 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { CarsComponent } from './cars/cars.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CarsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
